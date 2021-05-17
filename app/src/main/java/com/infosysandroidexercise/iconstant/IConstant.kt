f68803a4ced688420c0980cea30fc85a77671527
premkumarsan <premkumar.san@gmail.com>
2021-05-17 17:54:20 +0530

    Added Api Call

---
+++ app/src/main/java/com/infosysandroidexercise/iconstant/IConstant.kt
@@ -0,0 +1,9 @@
+package com.infosysandroidexercise.iconstant
+
+
+object IConstant {
+
+    const val BaseUrl = "https://dl.dropboxusercontent.com/s/"
+
+    const val dashboardUrl = "2iodh4vg0eortkl/facts.json"
+}
